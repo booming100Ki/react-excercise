51f65e5ed11e097a96967c7e8fa801a596716beb
booming100.ki <booming100.ki@gmail.com>
2020-11-01 13:41:06 +0900

    02. hello-react 완료

--- exercise/hello-react/src/App.js
+++ exercise/hello-react/src/App.js
@@ -1,14 +1,13 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-  const name = '리액트';
+  const name = "리액트";
 
   return (
     <>
-      <h1>{name} 안녕</h1>
-      <h2>잘 작동하니?</h2>
+      {/* 주석은 이렇게 작성합니다. */}
+      <div className="react">{name}</div>
     </>
-
   );
 }
 
