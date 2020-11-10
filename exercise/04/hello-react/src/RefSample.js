2135ab8eeb55502073bd6da4c0c5db875697410a
booming100Ki <booming100.ki@gmail.com>
2020-11-10 19:52:55 +0900

    chapter5. 클래스 컴포넌트 ref 정리 - ValidationSample.js, RefSample.js

---
+++ exercise/04/hello-react/src/RefSample.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class RefSample extends Component {
+  input = React.createRef();
+  handleFocus = () => {
+    this.input.current.focus();
+  }
+
+  render() {
+    return (
+      <>
+        <input ref={this.input} />
+      </>
+    )
+  }
+}
+
+export default RefSample;
