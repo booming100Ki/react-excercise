46ce68413e03d286e866d19b06fccac418c859b5
aki <kian@verygoodtour.com>
2020-11-30 08:53:51 +0900

    10. todo-app 프로젝트 추가 작업

--- exercise/todo-app/src/components/TodoList.js
+++ exercise/todo-app/src/components/TodoList.js
@@ -2,12 +2,12 @@ import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
-const TodoList = () => {
+const TodoList = ({ todos }) => {
   return (
-    <div className="TodoLIst">
-      <TodoListItem />
-      <TodoListItem />
-      <TodoListItem />
+    <div className="TodoList">
+      {todos.map(todo => (
+        <TodoListItem todo={todo} key={todo.id} />
+      ))}
     </div>
   );
 };
